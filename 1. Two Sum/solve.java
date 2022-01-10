e7a7a54f4a4c08f909701004f17eeea3518a4886
kumush <83897840+Kumushai9919@users.noreply.github.com>
2022-01-10 19:38:44 +0900

    Create solve.java

---
+++ 1. Two Sum/solve.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> nmap = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int complement = target-nums[i];
+            if(nmap.containsKey(complement)){
+                return new int[]{nmap.get(complement), i};
+            }
+            else{
+                nmap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
